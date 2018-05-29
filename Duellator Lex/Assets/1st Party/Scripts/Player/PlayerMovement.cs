c44a5e32b21c6fea2c0ca47497253789b9ab538b
Kaaz!b <kaazin.splatoon@gmail.com>
2018-05-28 21:31:02 -0400

    added starting assets
    added a character and animation pack and added started a player movment script

---
+++ Duellator Lex/Assets/1st Party/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public Vector3 direction;  //the player's direction of movment
+
+    public float speed;        //the speed at which the player should move
+
+    Rigidbody rb;       //the rigidbody attached to the player
+
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        float moveV = Input.GetAxis("Horizontal") * speed;
+
+
+        direction = new Vector3(0, direction.y, moveV);
+
+        rb.velocity = transform.rotation * direction;
+    }
+}
