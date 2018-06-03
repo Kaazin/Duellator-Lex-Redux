14934d4201b0eeec50cd1adcfd2693cd7e8cb936
Kaaz!b <kaazin.splatoon@gmail.com>
2018-06-02 23:21:09 -0400

    Started Main Menu and added hitboxees to the energy attacks

--- Duellator Lex/Assets/1st Party/Scripts/Misc/DetectHit.cs
+++ Duellator Lex/Assets/1st Party/Scripts/Misc/DetectHit.cs
@@ -8,21 +8,24 @@ public class DetectHit : MonoBehaviour
     public GameObject character;
     public bool player = true;
     public string name;
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () 
+    public GameObject Explosion;
+    // Use this for initialization
+    AudioSource hitSound;
+    void Start()
+    {
+        hitSound = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
     {
         //Debug.Log(this.transform.root.tag);
-	}
+    }
 
 
     void OnTriggerEnter(Collider col)
-	{
-        if(col.transform.root.tag != this.transform.root.tag && col.transform.tag == "Hurtbox" && !hit)
+    {
+        if (col.transform.root.tag != this.transform.root.tag && col.transform.tag == "Hurtbox" && !hit)
         {
             if (col.transform.root.name != name)
             {
@@ -36,69 +39,102 @@ public class DetectHit : MonoBehaviour
                     Debug.Log(playerHealth.gameObject.name);
                     playerHealth.TakeDamage(10, 1, 1);
                 }
-            }
-                
-                //Debug.Log(3);
-
-                if (!player)
-                {
-                    if (transform.root.tag != "VFX")
-                    {
-                        character.GetComponentInChildren<EnemyAI>().DisableLArmBoxes();
-                        character.GetComponentInChildren<EnemyAI>().DisableRArmBoxes();
-                        character.GetComponentInChildren<EnemyAI>().DisableLLegBoxes();
-                        character.GetComponentInChildren<EnemyAI>().DisableRLegBoxes();
-                    }
-
-                else if (col.transform.root.name != name)
-                     {
-                        GetComponent<SphereCollider>().enabled = false;
-                     }
-                }
-                else
-                {
-                    if (GetComponentInParent<EnemyAI>() != null)
-                        GetComponentInParent<EnemyAI>().attkHit = true;
-                    if (transform.root.tag != "VFX")
-                    {
-                        character.GetComponentInChildren<PlayerCombat>().DisableLArmBoxes();
-                        character.GetComponentInChildren<PlayerCombat>().DisableRArmBoxes();
-                        character.GetComponentInChildren<PlayerCombat>().DisableLLegBoxes();
-                        character.GetComponentInChildren<PlayerCombat>().DisableRLegBoxes();
-                    }
-                else if(col.transform.root.name != name)
-                {
+                if(hitSound != null)
+                hitSound.PlayOneShot(hitSound.clip);
+                if (GetComponent<SphereCollider>() != null)
                     GetComponent<SphereCollider>().enabled = false;
-                }
-
-                }
-
-                hit = true;
-
-                hit = false;
-
+                else
+                    GetComponent<BoxCollider>().enabled = false;
             }
 
+            //Debug.Log(3);
+
+            if (!player)
+            {
+                if (transform.root.tag != "VFX")
+                {
+                    character.GetComponentInChildren<EnemyAI>().DisableLArmBoxes();
+                    character.GetComponentInChildren<EnemyAI>().DisableRArmBoxes();
+                    character.GetComponentInChildren<EnemyAI>().DisableLLegBoxes();
+                    character.GetComponentInChildren<EnemyAI>().DisableRLegBoxes();
+                }
+
+   
+            }
             else
             {
-                PlayerHealth playerHealth = col.GetComponentInParent<PlayerHealth>();
-                DuoPlayerHealth duoPlayerHealth = col.GetComponentInParent<DuoPlayerHealth>();
-
-                if (col.transform.root.name != name)
+                if (GetComponentInParent<EnemyAI>() != null)
+                    GetComponentInParent<EnemyAI>().attkHit = true;
+                if (transform.root.tag != "VFX")
                 {
-                    if (duoPlayerHealth != null)
-                        duoPlayerHealth.TakeDamage(10, 1, 1);
-                    else if (playerHealth != null)
-                    {
-                        playerHealth.TakeDamage(10, 1, 1);
-                    }
+                    character.GetComponentInChildren<PlayerCombat>().DisableLArmBoxes();
+                    character.GetComponentInChildren<PlayerCombat>().DisableRArmBoxes();
+                    character.GetComponentInChildren<PlayerCombat>().DisableLLegBoxes();
+                    character.GetComponentInChildren<PlayerCombat>().DisableRLegBoxes();
+                }
+                else if (col.transform.root.name != name)
+                {
+                    GetComponent<SphereCollider>().enabled = false;
+                    Destroy(this);
                 }
 
-                Destroy(this);
             }
+
+            hit = true;
+
+            hit = false;
 
         }
 
-        //Debug.Log(1);
-}
+        else
+        {
+            PlayerHealth playerHealth = col.GetComponentInParent<PlayerHealth>();
+            DuoPlayerHealth duoPlayerHealth = col.GetComponentInParent<DuoPlayerHealth>();
 
+            if (col.transform.root.name != name)
+            {
+                if (duoPlayerHealth != null)
+                    duoPlayerHealth.TakeDamage(10, 1, 1);
+                else if (playerHealth != null)
+                {
+                    playerHealth.TakeDamage(10, 1, 1);
+                }
+            }
+
+            Destroy(this);
+        }
+
+    }
+
+
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.transform.name != name)
+        {
+            ContactPoint point = col.contacts[0];
+
+            Instantiate(Explosion, point.point, Quaternion.identity);
+
+            if (col.transform.root.name != name)
+            {
+                if (GetComponent<SphereCollider>() != null)
+                    GetComponent<SphereCollider>().enabled = false;
+                else
+                    GetComponent<BoxCollider>().enabled = false;
+                
+                PlayerHealth playerHealth = col.transform.GetComponentInParent<PlayerHealth>();
+                DuoPlayerHealth duoPlayerHealth = col.transform.GetComponentInParent<DuoPlayerHealth>();
+
+                if (duoPlayerHealth != null)
+                    duoPlayerHealth.TakeDamage(10, 1, 1);
+                else if (playerHealth != null)
+                {
+                    Debug.Log(playerHealth.gameObject.name);
+                    playerHealth.TakeDamage(10, 1, 1);
+                }
+            }
+
+        }
+        //Debug.Log(1);
+    }
+}
